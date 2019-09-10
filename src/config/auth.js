cec50e4385f5b5ebc2bd09007ab1b0eb512a10b2
Reginaldo Boeke <reginaldoboeke@gmail.com>
2019-09-10 18:58:56 -0300

    commit inicial

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'e7716c7f191a717b7f8daad539827a99',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Welcome to Meetapp API' });
+});
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
